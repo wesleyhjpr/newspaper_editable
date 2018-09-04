39131d97edb5365a010d00a8594c145383c33489
wesleyhjpr <wesleyhjpr@gmail.com>
2018-09-03 21:28:00 -0300

    fix logo, insert into navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,7 +47,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="https://www.valor.com.br/sites/all/themes/basic/images/logo-valor-economico-v2.png" alt="Valor Econômico." title="Valor Econômico.">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- resources/views/newspaper/page.blade.php
+++ resources/views/newspaper/page.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 <div class="container">
-    <img src="https://www.valor.com.br/sites/all/themes/basic/images/logo-valor-economico-v2.png" alt="Valor Econômico." title="Valor Econômico.">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="row justify-content-center">
